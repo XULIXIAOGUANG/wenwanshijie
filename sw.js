f71e22188bc6e47d11a943eb944e5606e88501d8
XULIXIAOGUANG <379469882@qq.com>
2026-08-25 02:51:12 +0800

    deploy v3.1.0: update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,10 @@
-const CACHE = 'wwapp-v3.1';
+const CACHE = 'wwapp-v3';
 const SHELL = [
   './index.html',
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
-  './icons/apple-touch-icon.png',
-  './assets/简洁的圆形用户头像占位图_文玩主题_米白色背景上放一枚精致的_2026-08-12T05-09-24.png',
-  './assets/人手盘玩文玩核桃的特写_手持两枚核桃在掌心转动_核桃表面红润_2026-08-14T03-37-55.png',
-  './assets/一对白狮子文玩核桃特写_皮质厚实_纹理清晰_红润包浆感_暖黄_2026-08-14T03-37-38.png',
-  './assets/真实感产品照片_一串紫红色小叶紫檀手串_摆放在米白色亚麻布上_2026-08-12T03-43-26.png',
-  './assets/真实感产品照片_一对文玩核桃_狮子头__表面有自然包浆光泽__2026-08-12T03-43-25.png'
+  './icons/apple-touch-icon.png'
 ];
 
 self.addEventListener('install', e => {
